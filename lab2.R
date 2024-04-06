75e8bdaf04ba4684932e89f11bee7d9ee12db276
Dinithi Sanjana <IT22074072@my.sliit.lk>
2024-04-06 09:52:03 +0530

    Questions

--- lab2.R
+++ lab2.R
@@ -94,3 +94,58 @@ function_01<-function(a,b){
 
 #calling the function
 function_01(4,6)
+
+
+
+#Questions
+#q1(Real roots for quadratic equation 2x^2+3x+1=0)
+quadRoots<-function(a,b,c){
+  x1=(-b+sqrt(b^2-4*a*c))/2*a
+  x2=(-b-sqrt(b^2-4*a*c))/2*a
+  print(x1)
+  print(x2)
+}
+
+quadRoots(2,3,1)
+
+#q2(indexes are starting from 1)
+x<-c(1,5,4)
+x[2]
+
+#q4
+#1:K
+vec1<-c(1:20) #3,6,9,12,15,18
+sum(vec1%%3==0)
+
+#q6
+data3<-c(24,67,45,23)
+max<-0
+maxIndex<-0
+
+for(i in 1:length(data3)){
+  if(max < data3[i]){
+    max <- data3[i]
+    maxIndex<- i
+  }
+}
+maxIndex
+
+#q7
+max(data3)
+which.max(data3)
+
+
+#q8
+A<-0
+moneyOwned<-function(P,R,n){
+  for(i in 1:n){
+    A<-P*(1 + R/100)^i
+    print(A)
+  }
+  
+  
+}
+moneyOwned(5000,11.5,15)
+
+
+
