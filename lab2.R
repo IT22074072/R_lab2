83407b7558264c6dc191005f0b1310180a142d97
Dinithi Sanjana <IT22074072@my.sliit.lk>
2024-04-06 07:09:54 +0530

    Conditional statements

---
+++ lab2.R
@@ -0,0 +1,55 @@
+getwd()
+
+#conditional statements
+#if
+x<-4
+x
+if(x>0){
+  print("Positive number")
+}
+
+#if-else
+x<-7
+y<--2
+z<-0
+x
+y
+z
+
+if(x>0){
+  print("Positive number")
+}else{
+  print("0 or Negative number")
+}
+
+
+#else if
+if(x>0){
+  print("Positive number")
+}else if(x==0){
+  print("zero")
+}else{
+  print("Negative number")
+}
+
+
+if(y>0){
+  print("Positive number")
+}else if(y==0){
+  print("zero")
+}else{
+  print("Negative number")
+}
+
+
+if(z>0){
+  print("Positive number")
+}else if(z==0){
+  print("zero")
+}else{
+  print("Negative number")
+}
+
+
+
+
